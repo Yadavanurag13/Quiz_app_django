2fde7c09cc515bce9a78ba36013e1f637325227c
Anurag Yadav <yadavanurag1310@gmail.com>
2024-05-04 20:28:34 +0530

    All models done

--- home/models.py
+++ home/models.py
@@ -1,11 +1,23 @@
 from django.db import models
-
+import uuid
 # Create your models here.
-
-class Category(models.Model):
-    category_name = models.CharField(max_length=100)
+class BaseModel(models.Model): 
+    uid = models.UUIDField(primary_key = True, default=uuid.uuid4, editable = False)
     createdAt = models.DateField(auto_now_add=True)
     updatedAt = models.DateField(auto_now=True)
 
-class Question(models.Model):
-    category = 
+    class Meta:
+        abstract = True
+
+class Category(BaseModel):
+    category_name = models.CharField(max_length=100)
+
+class Question(BaseModel):
+    category = models.ForeignKey(Category, related_name = 'Category', on_delete=models.CASCADE)
+    question = models.CharField(max_length=100)
+    marks = models.IntegerField(default=5)
+
+class Ans(BaseModel):
+    question = models.ForeignKey(Question, related_name = 'Question', on_delete=models.CASCADE)
+    ans = models.CharField(max_length=100)
+    isCorrect = models.BooleanField(default=False)
